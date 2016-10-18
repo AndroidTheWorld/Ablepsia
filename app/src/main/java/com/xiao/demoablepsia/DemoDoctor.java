36c7069e2f09da931163699c61251fe1bdca4483
Xiao <is.me.xiao@gmail.com>
2016-10-18 14:13:51 +0800

    Add files via upload

--- app/src/main/java/com/xiao/demoablepsia/DemoDoctor.java
+++ app/src/main/java/com/xiao/demoablepsia/DemoDoctor.java
@@ -1,5 +1,6 @@
 package com.xiao.demoablepsia;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
@@ -12,26 +13,31 @@ public class DemoDoctor extends Doctor {
     @NonNull
     @Override
     protected Anagraph treatment(String symptom, Context context, Class blind) {
+
         switch (symptom) {
             case "需要登录才能打开":
                 if (SignInStatus.isSign()){
                     return Anagraph.alive();// --不拦截，继续执行
                 }else {
                     Ablepsia.open(context,SignInActivity.class);
-                    return Anagraph.detained("先去登录");//  -- --拦截，直到调用 Anagraph.detainedCompleting("先去登录") 才会继续执行，我这里在 SignInActivity 里调用了 detainedCompleting
+//                    return Anagraph.detained("先去登录");//  -- --拦截，直到调用 Anagraph.detainedCompleting("先去登录") 才会继续执行，我这里在 SignInActivity 里调用了 detainedCompleting
 
                     //或者使用 Chaperonage 监视，直到当 true == Chaperonage()才继续执行
                     //默认每 500 毫秒监视一次
-//                    return Anagraph.detained(new Anagraph.Chaperonage() {
-//                        @Override
-//                        public boolean Chaperonage() {
-//                            return true; //         --继续执行
-//                        }
-//                    });
+                    return Anagraph.detained(new Anagraph.Chaperonage() {
+                        @Override
+                        public boolean Chaperonage() {
+                            return SignInStatus.isSign(); //         --继续执行
+                        }
+                    });
                 }
             default:
                 return Anagraph.die();// --拦截，终止跳转
         }
     }
 
+    @Override
+    protected void funeral(String symptom, Context context, Class<? extends Activity> activity) {
+        
+    }
 }
